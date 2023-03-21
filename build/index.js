1a084ab17f352baead98c78b6f710120dc2bc865
Paula Caroline Nascimento <pcaroline1998@gmail.com>
2023-03-21 17:32:05 -0300

    check typescript-i

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log(database_1.users, database_1.products, database_1.purchases);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { users, products, purchases } from "./database"
+
+console.log(users, products, purchases)
