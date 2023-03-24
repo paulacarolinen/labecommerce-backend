13c82bf860fd3cf70c2ef6f1eeb4ee61f2c0bb61
Paula Caroline Nascimento <pcaroline1998@gmail.com>
2023-03-24 01:57:49 -0300

    check typescript-ii

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { users, products, purchases } from "./database"
+import {
+  createUser,
+  getAllUsers,
+  createProduct,
+  getAllProducts,
+  getProductById,
+  queryProductsByName,
+  createPurchase,
+  getAllPurchasesFromUserId,
+} from "./database";
+import { PRODUCT_CATEGORY } from "./type";
 
-console.log(users, products, purchases)
+createUser("u01", "usuario@email.com", "xxxx");
+getAllUsers();
+createProduct("p004", "Monitor HD", 800, PRODUCT_CATEGORY.ELECTRONICS);
+getAllProducts();
+getProductById("p004");
+queryProductsByName("monitor");
+createPurchase("u003", "p004", 2, 1600);
+getAllPurchasesFromUserId("u003");
